331a85528702846d7a0bdec6c73b1f821f0e572e
jerico-digiteer <jerico.digiteer@gmail.com>
2024-09-14 17:56:07 +0800

    starting on products for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,51 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # Redirect to respective dashboards after sign-in
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_dashboard_path # Path to the admin dashboard
+    elsif resource.is_a?(User)
+      user_dashboard_path # Path to the user dashboard (Assuming this is defined)
+    else
+      super
+    end
+  end
+
+  # Redirect to respective dashboards after sign-up
+  def after_sign_up_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_dashboard_path 
+    elsif resource.is_a?(User)
+      user_dashboard_path 
+    else
+      super
+    end
+  end
+
+    # Redirect to respective dashboards after profile update
+    def after_update_path_for(resource)
+      if resource.is_a?(Admin)
+        admin_dashboard_path
+      elsif resource.is_a?(User)
+        user_dashboard_path
+      else
+        super
+      end
+    end
+
+  # Redirect to respective login pages after sign-out
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path # Redirect to the admin login page
+    elsif resource_or_scope == :user
+      new_user_session_path # Redirect to the user login page
+    else
+      super
+    end
+  end
+
+  # Configure additional permitted parameters for sign-up and account update
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :wallet])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone_number, :wallet])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_13_102101) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_14_095547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_13_102101) do
     t.decimal "wallet"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "variants"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
