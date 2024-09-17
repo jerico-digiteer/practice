a3d3f70d685e5a5a662f7cba02d42d37e12ebbcb
jerico-digiteer <jerico.digiteer@gmail.com>
2024-09-17 19:03:45 +0800

    reset javascript

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,15 +2,6 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-const context = require.context(".", true, /\.js$/)
-context.keys().forEach(key => {
-  if (key === './application.js') return // Skip itself
-  const controller = context(key)
-  const controllerName = key.replace(/^\.\/|\.js$/g, "")
-  application.register(controllerName, controller.default)
-})
-
-
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
