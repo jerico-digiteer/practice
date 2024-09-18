24ea45c05842df303cafc93903639a32bf9430be
jerico-digiteer <jerico.digiteer@gmail.com>
2024-09-18 08:58:18 +0800

    add variant still not working

---
+++ app/javascript/controllers/fields_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ["container", "template"];
+
+  add(e) {
+    e.preventDefault();
+    const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
+    this.containerTarget.insertAdjacentHTML('beforeend', content);
+  }
+
+  remove(e) {
+    e.preventDefault();
+    let item = e.target.closest('.nested-fields');
+    if (item.dataset.newRecord === "true") {
+      item.remove();
+    } else {
+      item.querySelector("input[name*='_destroy']").value = 1;
+      item.style.display = "none";
+    }
+  }
+}
