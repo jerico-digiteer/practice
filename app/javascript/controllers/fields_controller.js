b34bbe9d9b34e83622d8fee3a3552d7027c88772
jerico-digiteer <jerico.digiteer@gmail.com>
2024-09-18 15:45:38 +0800

    done demo

--- app/javascript/controllers/fields_controller.js
+++ app/javascript/controllers/fields_controller.js
@@ -1,12 +1,12 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["container", "template"];
+  static targets = ["container", "template", "addButton"];
 
   add(e) {
     e.preventDefault();
     const content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, new Date().getTime());
-    this.containerTarget.insertAdjacentHTML('beforeend', content);
+    this.addButtonTarget.insertAdjacentHTML('beforebegin', content);
   }
 
   remove(e) {
