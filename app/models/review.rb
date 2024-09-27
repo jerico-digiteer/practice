c420287a64cde5a2733e6dc07a7061613a10de8a
jerico-digiteer <jerico.digiteer@gmail.com>
2024-09-27 10:05:04 +0800

    added button

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,10 +9,8 @@ class Review < ApplicationRecord
   private
 
   def user_can_only_review_purchased_products
-    # Get product variant IDs for the current product
     product_variant_ids = product.product_variants.pluck(:id)
-
-    # Check if the current user has any orders with order items for these product variants
+# looks for user's exisiting orders if they have already ordered the product
     unless user.orders.joins(:order_items).where(order_items: { product_variant_id: product_variant_ids }).exists?
       errors.add(:base, "You can only review products you've purchased.")
     end
